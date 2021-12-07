d0c173659352b52e1ac7dffbcb78e3f54afb05af
Mel <tidyluna8@gmail.com>
2021-12-06 19:39:05 -0500

    finished color typing

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 import axios from "axios"
 let url = 'https://pokeapi.co/api/v2/pokemon/'
 
+const typeColors = {
+  normal:"#e0e0e0",
+  fighting:"#F0E8D1",
+  flying:"#A7B5D1",
+  poison:"#A977AA",
+  ground:"#C3A17A",
+  rock:"#C7B470",
+  bug:"#FCE09A",
+  ghost:"#BDAAE5",
+  steel:"#E3E6F1",
+  fire:"#ff8a8a",
+  water:"#ADCEFF",
+  grass:"#AAE396",
+  electric:"#FAF57A",
+  psychic:"#ff9cd1",
+  ice:"#C7F8FF",
+  dragon:"#B1ABFA",
+  dark:"#9C9FBE",
+  fairy:"#F7CBE5"
+}
+
 async function createCard() {
   for (let i = 1; i <= 150; i++){
     await axios.get(url + i)
@@ -31,6 +52,13 @@ async function createCard() {
       card_content.appendChild(title);
       card.appendChild(title);
 
+      //Color of Background
+      const main_types = Object.keys(typeColors);
+      const pokeType = data.types.map(type => type.type.name);
+      const type = main_types.find(type => pokeType.indexOf(type) == 0);
+      const color = typeColors[type];
+      card.style.backgroundColor = color;
+
 
       // const info = document.createElement('h2');
       // info.className = 'card_text';
@@ -48,3 +76,4 @@ async function createCard() {
 }
 
 createCard();
+
